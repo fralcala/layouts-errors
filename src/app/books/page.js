33c7b388a83f64e01b5f2ff1c88a461c30229f5f
Ty Streets <tstreets@iu.edu>
2024-11-06 10:09:57 -0500

    Added 404 page and layout for books nested routes

---
+++ src/app/books/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function BooksPage() {
+  return (
+    <main>
+      <h1>Books</h1>
+    </main>
+  );
+}
---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function PageNotFound() {
+  return (
+    <main>
+      <h1>Page Not Found</h1>
+      <p>Try using the right URL</p>
+    </main>
+  );
+}
